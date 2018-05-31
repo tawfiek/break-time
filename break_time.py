13f8faf0a44a7eb6baa87974991758bcc0e2b420
tawfiek <tawfiek.108@gmail.com>
2018-05-31 20:02:26 +0200

    first commmit

---
+++ break_time.py
@@ -0,0 +1,11 @@
+import webbrowser
+import time
+
+
+
+print("App starts at : " + time.ctime())
+while (1) : 
+    time.sleep(2*60*60)
+    webbrowser.open("https://soundcloud.com/stream")
+    i= i -1 
+
